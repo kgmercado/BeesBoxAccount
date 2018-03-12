971a3f1a3e180e8a9a385f9fd0092376d8f84674
Gail <Gail@DESKTOP-E08V7JH.local>
2018-03-12 12:39:03 +0800

    extends SpringBootServletInitializer as alternative to web.xml

--- src/main/java/org/bees/box/accounts/BeesBoxApplication.java
+++ src/main/java/org/bees/box/accounts/BeesBoxApplication.java
@@ -2,10 +2,17 @@ package org.bees.box.accounts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BeesBoxApplication {
+public class BeesBoxApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(BeesBoxApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BeesBoxApplication.class, args);
 	}
